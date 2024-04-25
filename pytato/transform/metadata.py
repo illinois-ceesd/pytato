7eefe6367432b209b7717bed7f50e42f5b78e2b6
Mike Campbell <mtcampbe@illinois.edu>
2024-04-25 07:37:41 -0500

    Add traceback print

--- pytato/transform/metadata.py
+++ pytato/transform/metadata.py
@@ -602,8 +602,13 @@ class AxisTagAttacher(CopyMapper):
                 assert expr_copy.ndim == expr.ndim
 
                 for iaxis in range(expr.ndim):
-                    expr_copy = expr_copy.with_tagged_axis(
-                        iaxis, self.axis_to_tags.get((expr, iaxis), []))
+                    axis_tags = self.axis_to_tags.get((expr, iaxis), [])
+                    if len(axis_tags) == 0:
+                        print("--------------------------------------------------------\n"
+                              f"Inferring axis({iaxis})_tag of array type {type(expr)}.\n"
+                              f"TraceBack:\n{expr.non_equality_tags=}\n"
+                              "--------------------------------------------------------")
+                    expr_copy = expr_copy.with_tagged_axis(iaxis, axis_tags)
 
                 # {{{ tag reduction descrs
 
